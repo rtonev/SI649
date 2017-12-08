1b7fe3947bb7167b6546e03053ddcd2acfae7677
Raden Tonev <rtonev@umich.edu>
2017-12-08 16:22:24 -0500

    loading some data

--- js/visualization.js
+++ js/visualization.js
@@ -1,254 +1,369 @@
+Array.prototype.max = function() {
+    return Math.max.apply(null, this);
+};
+
+
 $(document).ready(function() {
 
-    var colorChart = bb.generate({
-        "data": {
-            "columns": [
-                ["data1", 0, 0, 0, 5, 5, 5],
-                ["data2", 5, 5, 5, 0, 0, 0]
-            ],
-            "types": {
-                "data1": "area-step",
-                "data2": "area-step"
-            },
-            "colors": {
-                "data1": "red",
-                "data2": "blue"
-            }
-        },
-        "size": {
-                    "height": 550,
-                    "width": 800
+
+
+
+
+    function drawCharts(data) {
+        var colorChart = bb.generate({
+            "data": {
+                "columns": [
+                    ["data1", 0, 0, 0, 5, 5, 5],
+                    ["data2", 5, 5, 5, 0, 0, 0]
+                ],
+                "types": {
+                    "data1": "area-step",
+                    "data2": "area-step"
                 },
-
-        "axis": {
-            y: {
-                show:false
+                "colors": {
+                    "data1": "red",
+                    "data2": "blue"
+                }
             },
-            x: {
-                show:false
+            "size": {
+                "height": 550,
+                "width": 1200
+            },
+
+            "axis": {
+                y: {
+                    show:false
+                },
+                x: {
+                    show:false
+                }
+            },
+            "bindto": "#cabinetChart",
+            "legend": {
+                "show": false
+            },
+            "tooltip": {
+                "show": false
             }
-        },
-        "bindto": "#cabinetChart",
-        "legend": {
-            "show": false
-        },
-        "tooltip": {
-            "show": false
-        }
-    });
+        });
 
+        var avgDaysServedChart = new Chart($("#avgDaysServedChart"), {
+            type: 'line',
+            data : {
+                labels:data.Year,
 
-    var appointmentsChart = new Chart($("#appointmentsChart"), {
-        type: 'line',
+                datasets: [
+                    {
+                        borderWidth: 1,
+                        data:data.AvgDaysServed,
+                        borderColor:"gray",
+                        backgroundColor:"lightgray",
 
-        data : {
-            labels: ["2015","2016","2017"],
-
-            datasets: [
-                {
-                    data:[11,15,18],
-                    // fill: 'false',
-                    borderColor:"gray",
-                    backgroundColor:"lightgray"
-
-                }
-            ]
-        },
-        options: {
-            legend: {
-                display: false,
-            },
-            responsive: false,
-            title: {
-                fontSize:18,
-                display: true,
-                text: 'Number of Appointments per Cabinet'
-            },
-            label: {
-                display: false
-            },
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:false
-                    },
-                    gridLines: {
-                        display: false,
-                        color:"white"
-                    },
-
-                }],
-
-                xAxes: [{
-                    display: false
-                }]
-
-
-            }
-        }
-    });
-
-    var somethingElseChart = new Chart($("#somethingElseChart"), {
-        type: 'line',
-        data : {
-            labels: ["2015","2016","2017"],
-
-            datasets: [
-                {
-                    data:[5,2,18],
-                    borderColor:"gray",
-                    backgroundColor:"lightgray",
-
-                }
-            ]
-        },
-        options: {
-            legend: {
-                display: false,
-                position:"top"
-            },
-            responsive: false,
-            title: {
-                fontSize:18,
-                display: true,
-                text: 'Something Else Chart'
-            },
-            label: {
-                display: true,
-                labels: {
-                    boxWidth: 0
-                }
-            },
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:false
-                    },
-                    gridLines: {
-                        display: false,
-                        color:"white"
-                    },
-
-                }],
-
-                xAxes: [{
-                    gridLines: {
-                        display:true
-                    },
-                    display: false
-                }]
-
-
-            }
-        }
-    });
-
-    var yearsChart = new Chart($("#yearsChart"), {
-        type: 'line',
-        data : {
-            labels: ["2015","2016","2017"],
-            datasets: [
-                {
-                    data:[0,0,0],
-                    label: "",
-                    fill:"white",
-                    backgroundColor:"white",
-                    borderColor:"white"
-
-
-                }
-            ]
-        },
-        options: {
-            responsive: false,
-            title: {
-                display: false,
-                text: 'President'
-            },
-            legend: {
-                display:false
-            },
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    },
-                    display:false
-
-
-                }],
-
-                xAxes: [{
-                    display: true,
-                    gridLines: {
-                        display: false,
-                        color:"white"
                     }
+                ]
+            },
+            options: {
+                elements: {
+                    point: {
+                        radius: 0
+                    },
+                    line: {
+                        tension: 0
+                    }
+                },
+                legend: {
+                    display: false,
+                },
+                responsive: false,
+                title: {
+                    fontSize:16,
+                    display: true,
+                    text: 'Average Days Served'
+                },
+                label: {
+                    display: false
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true,
+                            // max: 20,
+                            // stepSize: 1
+                        },
+                        gridLines: {
+                            display: false,
+                            color:"white"
+                        },
 
-                }]
 
+                    }],
 
-            }
+                    xAxes: [{
+                        display: false
+                    }]
 
-        }
-    });
-
-    var presidentChart = new Chart($("#presidentChart"), {
-        type: 'line',
-        data : {
-            labels: ["Obama","","Trump"],
-            datasets: [
-                {
-                    data:[0,0,0],
-                    label: "",
-                    fill:"white",
-                    backgroundColor:"white",
-                    borderColor:"white"
 
                 }
-            ]
-        },
-        options: {
-            responsive: false,
-            title: {
-                display: false,
-                text: 'President'
-            },
-            legend: {
-                display:false,
-            },
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:false
-                    },
-                    display:false
+            }
+        });
 
+        var appointmentsChart = new Chart($("#appointmentsChart"), {
+            type: 'line',
 
-                }],
+            data : {
+                labels: data.Year,
 
-                xAxes: [{
-                    display: true,
-                    gridLines: {
-                        display: false,
-                        color:"white"
-                    },
-                    ticks: {
-                        color:"white"
+                datasets: [
+                    {
+                        borderWidth: 1,
+                        data:data.NumAppointments,
+                        // fill: 'false',
+                        borderColor:"gray",
+                        backgroundColor:"lightgray"
+
                     }
-                }]
+                ]
+            },
+            options: {
+                elements: {
+                    point: {
+                        radius: 0
+                    },
+                    line: {
+                        tension: 0
+                    }
+                },
+                legend: {
+                    display: false,
+                },
+                responsive: false,
+                title: {
+                    fontSize:16,
+                    display: true,
+                    text: 'Number of Appointments per Cabinet'
+                },
+                label: {
+                    display: false
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true,
+                            // max: 20,
+                            // stepSize: 1
+                        },
+                        gridLines: {
+                            display: false,
+                            color:"white"
+                        },
 
+
+                    }],
+
+                    xAxes: [{
+                        display: false
+                    }]
+
+
+                }
+            }
+        });
+
+        var appointmentsChart2 = new Chart($("#2appointmentsChart"), {
+            type: 'line',
+
+            data : {
+                labels: data.Year,
+
+                datasets: [
+                    {
+                        borderWidth: 1,
+                        data:data.NumAppointments,
+                        // fill: 'false',
+                        borderColor:"gray",
+                        backgroundColor:"lightgray"
+
+                    }
+                ]
+            },
+            options: {
+                elements: {
+                    point: {
+                        radius: 0
+                    },
+                    line: {
+                        tension: 0
+                    }
+                },
+                legend: {
+                    display: false,
+                },
+                responsive: false,
+                title: {
+                    fontSize:16,
+                    display: true,
+                    text: 'Number of Appointments per Cabinet'
+                },
+                label: {
+                    display: false
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true,
+                            // max: 20,
+                            // stepSize: 1
+                        },
+                        gridLines: {
+                            display: false,
+                            color:"white"
+                        },
+
+
+                    }],
+
+                    xAxes: [{
+                        display: false
+                    }]
+
+
+                }
+            }
+        });
+
+
+
+        var yearsChart = new Chart($("#yearsChart"), {
+            type: 'line',
+            data : {
+                labels: data.Year,
+                datasets: [
+                    {
+                        data:data.Zero,
+                        label: "",
+                        fill:"white",
+                        backgroundColor:"white",
+                        borderColor:"white"
+
+
+                    }
+                ]
+            },
+            options: {
+                responsive: false,
+                title: {
+                    display: false,
+                    text: 'President'
+                },
+                legend: {
+                    display:false
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        },
+                        display:false
+
+
+                    }],
+
+                    xAxes: [{
+                        display: true,
+                        gridLines: {
+                            display: false,
+                            color:"white"
+                        }
+
+                    }]
+
+
+                }
 
             }
+        });
+
+        var presidentChart = new Chart($("#presidentChart"), {
+            type: 'line',
+            data : {
+                labels: data.Administration,
+                datasets: [
+                    {
+                        data:data.Zero,
+                        label: "",
+                        fill:"white",
+                        backgroundColor:"white",
+                        borderColor:"white"
+
+                    }
+                ]
+            },
+            options: {
+                responsive: false,
+                title: {
+                    display: false,
+                    text: 'President'
+                },
+                legend: {
+                    display:false,
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:false
+                        },
+                        display:false
+
+
+                    }],
+
+                    xAxes: [{
+                        display: true,
+                        gridLines: {
+                            display: false,
+                            color:"white"
+                        },
+                        ticks: {
+                            color:"white"
+                        }
+                    }]
+
+
+                }
+
+            }
+        });
+    }
+
+    $.getJSON("data/american-cabinet.json", function(jsonArray) {
+        var clonedArray = JSON.parse(JSON.stringify(jsonArray));
+
+        var data = {
+            NumAppointments: [],
+            Year: [],
+            AvgDaysServed: [],
+            SenateMajorityA: [],
+            SenateMajorityB: [],
+            Zero: [],
+            Administration: []
+        };
+
+        for (var index=0; index < clonedArray.length; index++) {
+            var row = jsonArray[index];
+            data.NumAppointments.push(row.NumAppointments);
+            data.Year.push(row.Year);
+            data.AvgDaysServed.push(row.AvgDaysServed);
+            data.Zero.push(0);
+            data.Administration.push(row.Administration);
 
         }
-    });
+
+        drawCharts(data);
 
 
-    $.getJSON("data/american-cabinet.json", function(json) {
-        console.log(json[226]);
-        console.log(json[227]);
-        console.log(json[228]);
 
+
+
+        console.log(jsonArray[226]);
     });
 });
